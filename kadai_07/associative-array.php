6ed5cee973f681ec1f3d4b1dd744dfb256a845f3
Ryusuke9248 <146909431+Ryusuke9248@users.noreply.github.com>
2024-03-05 00:43:18 +0900

    Add files via upload

---
+++ kadai_07/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang = "ja">
+  <head>
+    <meta charset = "utf-8">
+    <title>PHP基礎編_kadai_007</title>
+  </head>
+
+  <body>
+    <?php
+    $array = ['name' => 'onion', 'price' => '200', 'weight'=>'160'];
+    print_r($array);
+
+    ?>
+  </body>
+</html>
